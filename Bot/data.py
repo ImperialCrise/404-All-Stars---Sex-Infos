f268c111252919c268bca4ecd8006a95d81b5dad
Noe <Guynoedjoufaing@gmail.com>
2022-12-02 04:17:37 +0100

    feat : ajout du parseur des reponse du bot (parser.py / data.py)

---
+++ Bot/data.py
@@ -0,0 +1,49 @@
+matching_word = {
+  'risque':'ris',
+  'guerir':'guer',
+  'question' : 'quest',
+  'transmet' : 'transm',
+  'vih':'vih',
+  'sida':'sid',
+  'seropositif' : 'sero',
+  'embrasser' : 'embrasser',
+  'mère': 'mere',
+  'parent':'pare',
+  'enfant': 'enf',
+  'bebe' : 'bebe',
+  'prevention' : 'prev',
+  'soigner':'soig',
+  'traitement' : 'traite',
+  'symptomes' : 'sympt', 
+	'attaque' : 'attaq', 
+  'organisme' : 'organ', 
+	'est' : 'est' , 
+  'sont' : 'sont',
+	'comment':'comme' ,
+}
+
+question_reponse = {
+  'risque' : ('Vous avez été exposé-e à un accident d\'exposition sexuelle  et à un risque de transmission du VIH ?\n\nNe perdez pas de temps : un traitement post-exposition (TPE) appelé aussi traitement prophylactique) peut empêcher une contamination. \n\nRendez-vous au service des urgences d’un hôpital le plus rapidement possible, de préférence dans les 4 premières heures, au plus tard dans les 48 heures'
+, 'https://www.sida-info-service.org/risque-moins-de-48-heures'),
+
+  'guerir' : ('La lutte contre le virus du SIDA est toujours menée avec acharnement par les scientifiques. S’il existe aujourd’hui des traitements permettant aux patients de mieux vivre avec le SIDA, de nouvelles données scientifiques pourraient permettre l’exploration d’autres voies thérapeutiques. \n \nComment agit le traitement actuel contre le virus du SIDA ?', 'https://info.medadom.com/sante_decomplexee/guerir-sida-vih'),
+
+  'embrasser' : ('L’infection par le VIH (virus responsable du sida) est transmissible dans certaines circonstances très précises. Le plus souvent elle se produit lors de rapports sexuels non protégés. Ce n’est pas une maladie contagieuse qui s’attraperait sans contact direct, comme la grippe.', 'https:/www.sida-info-service.org/generalites-sur-la-transmission-du'),
+
+'parent' : ('Il n’existe pas de transmission du VIH père-enfant sans que la mère soit elle-même infectée, toutefois la transmission peut avoir lieu de entre la mère et l’enfant en cas d’absence de traitement de la mère, ou pendant l’allaitement', 'https://www.sida-info-service.org/quel-est-le-risque-de-transmission498'),
+
+'transmission' : ('Les modes de transmission du virus VIH sont :Des rapports sexuels non protégés s’il y a pénétration vaginale, anale ou buccale\n\nUn contact important avec du sang contaminé lors de partage de matériel d’injection ou en cas d’accident d’exposition (pour les soignants)\nLa transmission de la mère à l’enfant en cas d’absence de traitement de la mère, ou pendant l’allaitement'
+,'https://www.sidaction.org/transmission-vih-sida'),
+
+'prevention' : ('Contre l’infection par le VIH, il existe différents moyens pour se protéger et protéger autrui.\n\nSelon le moment, les pratiques sexuelles, leur fréquence, l’un ou l’autre de ces moyens pourra être préféré ou plus adapté. rendez vous sur le site pour plus d\'information' ,'https://info-vih.com/prevention'),
+
+'soigner' : ('L’objectif principal du traitement antirétroviral (ARV) est d’empêcher la progression de l’infection,en particulier son évolution vers le sida, en maintenant ou en restaurant le système immunitaire.\nD’autres objectifs doivent être recherchés simultanément :\nla meilleure tolérance possible, clinique et biologique, à court, moyen et long termes, l’amélioration ou la préservation de la qualité de vie, la réduction du risque de transmission du VIH.', 'https://www.sidainfoplus.fr/apres-lannonce/le-traitement-contre-le-vih'),
+
+  'symptomes' : ('Attention : il est impossible de se baser sur des symptômes pour affirmer que quelqu’un a le VIH – et ce quels que soient les symptômes.\nCe n\'est qu\'après un test de dépistage du VIH positif qu\'on pourra établir si des symptômes sont dus  à une contamination par le VIH.\nQuand une personne a été contaminée par le VIH certains symptômes peuvent apparaitre entre 5 et 30 jours après la contamination.', 'https://www.sida-info-service.org/j-ai-des-symptomes-c-est-le-sida'),
+
+  'attaque' : ('Une fois le virus introduit, il s\'attaquera aux globules blancs. On devient malade parce que les anticorps ne sont pas capables d’éliminer la totalité des virus présents, et parce que, au bout d’un certain temps, variable selon les personnes, le renouvellement des lymphocytes est de moins en moins important (l’organisme aussi s’épuise). Leur nombre devient insuffisant et le système immunitaire ne fonctionne plus correctement.', 'https:/www.sida-info-service.org/le-vih-et-le-systeme-immunitaire/'),
+
+'question' : ('Pour toute question concernant le SIDA, j’appelle ','https:/www.vih-info-soignants.fr/'),
+
+  'sida' : ('Decouvrir ce qu\'est le sida', 'https://www.planning-familial.org/fr/le-sida-quest-ce-que-cest-358')
+}
